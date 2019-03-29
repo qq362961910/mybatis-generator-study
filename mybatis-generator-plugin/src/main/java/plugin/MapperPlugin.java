a0df4b0fb0566727011eb72ae512265bc76bc7dc
yangjian <yangjian>
2019-03-29 17:14:49 +0800

    配置优化，mapper可继承父类

---
+++ mybatis-generator-plugin/src/main/java/plugin/MapperPlugin.java
@@ -0,0 +1,36 @@
+package plugin;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+public class MapperPlugin extends PluginAdapter {
+
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        //主键默认采用java.lang.Long
+        FullyQualifiedJavaType fqjt = new FullyQualifiedJavaType("BaseMapper<"
+            + introspectedTable.getBaseRecordType() + ","
+            + introspectedTable.getExampleType() + ","
+            + "java.lang.Long" + ">");
+        FullyQualifiedJavaType imp = new FullyQualifiedJavaType(
+            "com.study.security.dao.BaseMapper");
+        //添加 extends MybatisBaseMapper
+        interfaze.addSuperInterface(fqjt);
+
+        //添加com.study.security.dao.BaseMapper
+        interfaze.addImportedType(imp);
+        //方法不需要
+        interfaze.getMethods().clear();
+        interfaze.getAnnotations().clear();
+        return true;
+    }
+}
