92b97d3ed1cdad664edfc71763eb325e5ed2ad79
yangjian <yangjian>
2019-04-25 22:18:16 +0800

    insert该在完毕

--- mybatis-generator-plugin/src/main/java/generator/ExtendedXMLMapperGenerator.java
+++ mybatis-generator-plugin/src/main/java/generator/ExtendedXMLMapperGenerator.java
@@ -1,20 +1,36 @@
 package generator;
 
+import mybatis3.ExtendedInsertElementGenerator;
+import mybatis3.ExtendedInsertSelectiveElementGenerator;
 import org.mybatis.generator.api.dom.xml.XmlElement;
 import org.mybatis.generator.codegen.mybatis3.xmlmapper.XMLMapperGenerator;
 import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
-import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.InsertSelectiveElementGenerator;
 
 /**
  * Xml文件生成器扩展，被ExtendedJavaMapperGenerator创建
  * */
 public class ExtendedXMLMapperGenerator extends XMLMapperGenerator {
 
+
+    @Override
+    protected void addInsertElement(XmlElement parentElement) {
+        if (introspectedTable.getRules().generateInsert()) {
+            if (introspectedTable.getPrimaryKeyColumns().size() > 1) {
+                AbstractXmlElementGenerator elementGenerator = new ExtendedInsertElementGenerator(false);
+                initializeAndExecuteGenerator(elementGenerator, parentElement);
+            }
+        }
+    }
+
     @Override
     protected void addInsertSelectiveElement(XmlElement parentElement) {
         if (introspectedTable.getRules().generateInsertSelective()) {
-            AbstractXmlElementGenerator elementGenerator = new InsertSelectiveElementGenerator();
-            initializeAndExecuteGenerator(elementGenerator, parentElement);
+            if (introspectedTable.getPrimaryKeyColumns().size() > 1) {
+                AbstractXmlElementGenerator elementGenerator = new ExtendedInsertSelectiveElementGenerator();
+                initializeAndExecuteGenerator(elementGenerator, parentElement);
+            }
         }
     }
+
+
 }
